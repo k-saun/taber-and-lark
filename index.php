d6cdd1faf08e18137b910aaf3857a206cb72795a
Keegan Saunders <keegansaun@gmail.com>
2019-09-29 12:58:49 -0600

    refactor+navbar

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once("home.html") ?>
+<html>
+  <head>
+	<title>T&L</title>
+	<link rel="stylesheet" type="text/css" href="/style/style.css">
+  </head>
+    <body>
+    	<?php include_once("navbar.php") ?>
+	</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,11 @@
+<ul id="navbar">
+	<li>
+	  <div id="burger">
+  		<div class="burger-bar"></div>
+  		<div class="burger-bar"></div>
+  		<div class="burger-bar"></div>
+	  </div>
+	</li>
+	<li></li>
+	<li></li>
+</ul>
