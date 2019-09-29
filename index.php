542bac441f05d29d3866fcb2847c76585539c555
Keegan Saunders <keegansaun@gmail.com>
2019-09-29 16:24:36 -0600

    adding item image grid

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 	<link rel="stylesheet" type="text/css" href="/style/style.css">
   </head>
     <body>
+
 		<?php include_once("navbar.php") ?>
 		<div class="main">
 			<div id="enter">
+				// on button press, item grid will appear
     			<button type="button" >Enter Shop</button>
     		</div>
+    		<?php include_once("items.php")>
     	</div>
 	</body>
 </html>
---
+++ style/items.php
@@ -0,0 +1,16 @@
+<div class="item-grid">
+	<div class="item-row">
+		<div column="item-column">
+			<img src="/images/jewlery.png">
+			<img src="/images/jewlery.png">
+			<img src="/images/jewlery.png">
+			<img src="/images/jewlery.png">
+		</div>
+		<div column="item-column">
+			<img src="/images/jewlery.png">
+			<img src="/images/jewlery.png">
+			<img src="/images/jewlery.png">
+			<img src="/images/jewlery.png">
+		</div>
+	</div>
+</div>
