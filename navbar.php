0865c29eef0c9f99ff6ed8752b888a0fbc6402c5
Keegan Saunders <keegansaun@gmail.com>
2019-09-30 15:51:54 -0600

    i guess nothing made it in the commit

--- navbar.php
+++ navbar.php
@@ -4,11 +4,13 @@
   		<div class="burger-bar"></div>
   		<div class="burger-bar"></div>
 	</div>
+
 	<div class="center-container">
 	  <a href="index.php">
 	    <img src="/images/temp-sprite.png" alt="logo">
 	  </a>	
 	</div>
+	
 	<div> 
 		<div style="text-align: right">cart logo here</div>
 	</div>
