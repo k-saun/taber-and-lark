75b76a9f07c1c1f5d384ab374364d5faad4290fa
Keegan Saunders <keegansaun@gmail.com>
2019-09-30 15:38:45 -0600

    navbar.php refactor

--- navbar.php
+++ navbar.php
@@ -1,19 +1,15 @@
-<ul id="navbar">
-	<li>
-	  <div>
+<div id="navbar">
+	<div>
   		<div class="burger-bar"></div>
   		<div class="burger-bar"></div>
   		<div class="burger-bar"></div>
-	  </div>
-	</li>
-	<li>
-		<div class="center-container">
-		  <a href="index.php">
-		    <img src="/images/temp-sprite.png" alt="logo">
-		  </a>	
-		</div>
-	</li>
-	<li> 
+	</div>
+	<div class="center-container">
+	  <a href="index.php">
+	    <img src="/images/temp-sprite.png" alt="logo">
+	  </a>	
+	</div>
+	<div> 
 		<div style="text-align: right">cart logo here</div>
-	</li>
-</ul>
+	</div>
+</div>
