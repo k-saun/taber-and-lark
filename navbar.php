12ef6d6c678963c61320dffdd0e8cf14ca70b372
Keegan Saunders <keegansaun@gmail.com>
2019-09-30 17:20:35 -0600

    navbar refactor

--- navbar.php
+++ navbar.php
@@ -1,17 +1,17 @@
 <div id="navbar">
-	<div>
+	<div class="navbar-section">
   		<div class="burger-bar"></div>
   		<div class="burger-bar"></div>
   		<div class="burger-bar"></div>
 	</div>
 
-	<div class="center-container">
+	<div class="navbar-section center-container">
 	  <a href="index.php">
 	    <img src="/images/temp-sprite.png" alt="logo">
 	  </a>	
 	</div>
 	
-	<div> 
+	<div class="navbar-section"> 
 		<div style="text-align: right">cart logo here</div>
 	</div>
 </div>
